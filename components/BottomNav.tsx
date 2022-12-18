30632a028b53138f935ed12f3f86d9e908f1ff3a
jason-warner <thupermn@protonmail.com>
2022-12-18 01:30:10 -0500

    responsive botton nav dependent on current page is done

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -3,39 +3,34 @@ import style from '../styles/BottomNav.module.scss';
 import Link from 'next/link'
 
 const BottomNav = () => {
-    const [pages, setPages] = React.useState([
-        'Services',
-        'Gallery',
-        'Contact'
-    ]);
-
+    const [urlPages, setUrlPages] = React.useState({
+        'Services': 'services',
+        'Gallery': 'gallery',
+        'Contact': 'contact'
+    });
+    
     const [currentUrl, setCurrentUrl] = React.useState('');
 
-    // const handleNavChange = (currentUrl: string) => {
-    //     let newPages = pages;
-    //     pages.forEach((page) => {
-    //         if ((page.toLowerCase()) )
-    //     })
-    //     return setPages(newPages)
-    // }
+    const handleNavChange = () => {
+        for (const page in urlPages) { //@ts-ignore
+            if (currentUrl.includes(urlPages[page].toLowerCase())) {
+                const newPages = { ...urlPages, 'Home': 'https://hustlinlawncare.com' };  //@ts-ignore
+                delete newPages[page];
+                return setUrlPages(newPages);
+            }
+        };
+    }
 
     React.useEffect(() => setCurrentUrl(window.location.href), []);
-
-    // React.useEffect(() => console.log(currentUrl), [currentUrl]);
+    React.useEffect(() => handleNavChange(), [currentUrl]);
 
     return (
         <div className={style.container}>
-            {pages.map((page, index) => {
-                // console.log('location: ', location.pathname);
-                // const path = location.pathname;
-                // let shouldAddHome = false;
-                // pages.forEach((page) => )
-                // const newPages = location.pathname.includes('')
+            {Object.keys(urlPages).map((page, index) => {
+
                 return (
-                    <Link className={style.option} key={index} passHref href={`/${page.toLowerCase()}`}>
-
+                    <Link className={style.option} key={index} passHref href={`/${page === 'Home' ? '' : page.toLowerCase()}`}>
                         {page}
-
                     </Link>
                 )
             })}
@@ -44,16 +39,3 @@ const BottomNav = () => {
 }
 
 export default BottomNav;
-
-
-
-{/* <Link href='/' passHref>
-<a className={style.option}> Services </a>
-</Link>
-<Link href='/' passHref>
-
-<a className={style.option}> Gallery </a>
-</Link>
-<Link href='/' passHref>
-<a className={style.option}> Contact </a>
-</Link> */}
